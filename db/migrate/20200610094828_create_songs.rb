9f8ee303709eb82e10f1f925d2608107844337dc
Juan Carlo Banayo <johncban@gmail.com>
2020-06-10 12:16:21 +0000

    Automatically backed up by Learn

--- db/migrate/20200610094828_create_songs.rb
+++ db/migrate/20200610094828_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :title
-      t.boolean :released
+      t.boolean :release
       t.integer :release_year
       t.string :artist_name
       t.string :genre
