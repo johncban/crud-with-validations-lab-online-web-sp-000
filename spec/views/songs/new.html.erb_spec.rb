7d2bcf4b121db9bc4302fcdc97638f1d3edac102
Juan Carlo Banayo <johncban@gmail.com>
2020-06-10 11:43:27 +0000

    Automatically backed up by Learn

--- spec/views/songs/new.html.erb_spec.rb
+++ spec/views/songs/new.html.erb_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+RSpec.describe 'songs/new', type: :feature do
+  it 'renders form' do
+    visit new_song_path
 
-RSpec.describe "songs/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+    form = find('form')
+
+    expect(form.find('input#song_title')[:name]).to eq('song[title]')
+    expect(form.find('input#song_release_year')[:name]).to eq('song[release_year]')
+    expect(form.find('input#song_released')[:name]).to eq('song[released]')
+    expect(form.find('input#song_genre')[:name]).to eq('song[genre]')
+    expect(form.find('input#song_artist_name')[:name]).to eq('song[artist_name]')
+  end
 end
